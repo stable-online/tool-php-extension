4be9812f6e76646accc5e31f9912ffb617b2a128
zw <2426595849@qq.com>
2022-01-13 18:30:20 +0800

    [lower] add testing {10}

--- tools.php
+++ tools.php
@@ -1,7 +1,4 @@
 <?php
 
 $str = new String("123456");
-echo $str->insert_head("--bb--")->substr(2)->insert_tail("--hello")->replace("hello","notlala")->value();
-//   echo $str->substr(1,3)->dump().PHP_EOL;//output:123
-//   echo $str->replace('23',"bbb")->dump();//1bbb456
-//   echo $str->substr(1,3)->dump();//output:23
+echo $str->insert_head("--bbBBBBBCCC--")->substr(2)->insert_tail("--hello")->replace("hello","notlala")->lower()->value();
